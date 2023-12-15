d87df1aa1144c8a7fe4f95971107675b9383ae0b
Kaisamira <thienthan726@gmail.com>
2023-12-15 11:56:32 +0700

    upload: upload project

---
+++ src/main/java/com/alpha/mangaapplication/MangaApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.mangaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaApplication.class, args);
+    }
+
+}
